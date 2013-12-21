5d0a64a6e6e6a74cc43a0e206c5c618a8fdb2a20
shakeer <mdshakeer91@gmail.com>
2013-12-21 15:51:35 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlaceSearch::Application.config.secret_key_base = 'bb2e48d1adda80f0a3ca84e213f3b7b52bf05e3dbd739d462189f553ce33911ba0b397ed19c010cb701c0c5d2f4ebbcfb26b0e58f6c24e5df1971844b509598d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlaceSearch::Application.config.session_store :cookie_store, key: '_place-search_session'
